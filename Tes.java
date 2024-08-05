50fc1e50c30f216e8f6c63e7c69a77cb1b24f711
graceistr1nyabakhen0l <graciella_angelia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 14:12:24 +0700

    Update

---
+++ Tes.java
@@ -0,0 +1,6 @@
+public class Tes {
+    public static void main(String[] args) {
+    string nama = = "Grace";
+    string kelas = "X RPL 5";
+    System.out.print("halo, nama saya" + grace + " dari kelas " + x RPL 5);
+      }
